1623943ac3ca176d4cf5beb0bb79178a9e46d54c
Junichi Hashimoto <sou3ilow@gmail.com>
2024-01-08 17:11:48 +0900

    upload part

---
+++ notionif.py
@@ -0,0 +1,60 @@
+import asyncio
+from notion_client import AsyncClient
+from pprint import pprint
+
+import myenv
+# myenv.PAGE_ID
+# myenv.NOTION_TOKEN
+
+notion = None
+page_id = None
+
+def init(auth=None, target_id=None):
+    global notion
+    global page_id
+    page_id = target_id or myenv.PAGE_ID
+
+    print(f"target page id is {page_id}")
+    auth = auth or myenv.NOTION_TOKEN
+
+    print(page_id)
+    print(auth)
+ 
+    notion = AsyncClient(auth=auth)
+    #pprint(await notion.users.list())
+
+async def upload(text):
+    #print("uploading....")
+    response = await notion.blocks.children.append(
+        block_id = page_id,
+        children = [{
+            "object": "block",
+            "type": "bulleted_list_item", "bulleted_list_item":
+            #"type": "paragraph", "paragraph":
+            {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": text
+                    }
+                }]
+            }
+        }]
+    )
+    #print("dene")
+
+
+async def main():
+   await init() 
+   await update("hello")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    
+    
+    
+
+
+
