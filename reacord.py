2cf883b20578b3888051efb8fda974826efabdda
Junichi Hashimoto <sou3ilow@gmail.com>
2024-01-08 17:06:48 +0900

    inital chain: recording ~ trascription ~ upload

--- reacord.py
+++ reacord.py
@@ -3,120 +3,212 @@ import pyaudio
 import numpy as np
 from pynput import keyboard
 import wave
+import threading
+import queue
+import asyncio
+
+#import myenv
+import whisperso
+import notionif
 
 # オーディオストリーム設定
 FORMAT = pyaudio.paInt16  # オーディオフォーマット（例：16ビットPCM）
-SAMPLE_WIDTH = 2 # HACK これはpaInt16を使う場合の値。 ref. pyaudio.get_sample_size(format)
+#FORMAT = pyaudio.paFloat32
+#SAMPLE_WIDTH = 2         # HACK これはpaInt16を使う場合の値。 ref. pyaudio.get_sample_size(format)
 CHANNELS = 1              # チャンネル数（モノラル）
-#RATE = 44100              # サンプリングレート（例：44.1kHz）
-RATE = 16000
-#CHUNK = 1024              # チャンクサイズ（一度に読み取るフレーム数）
-CHUNK = 2048
-input_device_index = 0
+#SAMPLE_RATE = 44100      # サンプリングレート（例：44.1kHz）
+SAMPLE_RATE = 16000
+#CHUNK_SIZE = 1024        # チャンクサイズ（一度に読み取るフレーム数）
+CHUNK_SIZE = 2048
+#gInputDeviceIndex = 0
+gInputDeviceIndex = 3
 
 # 無音判定の閾値と持続時間
-#SILENCE_THRESHOLD = 0.02  # 無音と判定する音量の閾値
-SILENCE_THRESHOLD = 500
-SILENCE_DURATION = 1.5      # 無音と判定する持続時間（秒）
+#SILENCE_THRESHOLD = 0.02 # 無音と判定する音量の閾値
+#SILENCE_THRESHOLD = 500   #for int16?
+SILENCE_THRESHOLD = 500   #for int16?
+SILENCE_DURATION = 0.5    # 無音と判定する持続時間（秒）
 
-
-
-isRecording = True
+gIsRecording = True
 
 # キーボードをチェックして状態を変更
 def on_press(key):
-    global isRecording
+    global gIsRecording
     try:
         if ( key.char == 'q' ):
-            isRecording = False
+            gIsRecording = False
             print("q is pressed")
-            
     except AttributeError:
         print('special key {0} pressed'.format(key))
-    
-    
+        gIsRecording = False
 
-def recorder():
-    global isRecording
-
+# 音声取得
+def record(task_queue):
     # PyAudioインスタンスの初期化
     audio = pyaudio.PyAudio()
 
     # オーディオストリームの開始
     stream = audio.open(format=FORMAT, channels=CHANNELS,
-                        rate=RATE, input=True,
-                        input_device_index=input_device_index,
-                        frames_per_buffer=CHUNK)
+                        rate=SAMPLE_RATE, input=True,
+                        input_device_index=gInputDeviceIndex,
+                        frames_per_buffer=CHUNK_SIZE)
 
     # オーディオデータを格納するバッファ
     audio_buffer = []
     silence_start = None
 
+    seqnum = 0
+    status = 'A_waiting_start'
+    voice_off_count = 0
+
+    Threshold = SILENCE_DURATION * SAMPLE_RATE / CHUNK_SIZE
+
     # オーディオのキャプチャループ
-    while isRecording:
+    while gIsRecording:
 
         # オーディオデータの読み取り
-        data = stream.read(CHUNK)
-        audio_buffer.append(data)
+        data = stream.read(CHUNK_SIZE)
 
         # 音量レベルの計算
         amplitude = np.frombuffer(data, dtype=np.int16)
         volume = np.abs(amplitude).mean()
+        voice_on = volume > SILENCE_THRESHOLD
+        #print(f"{volume}");
 
-
-        # 無音区間の検出
-        if volume < SILENCE_THRESHOLD:
-            print(".", end='', flush=True)
-
-            time_in_buff = len(audio_buffer) * CHUNK / RATE
-
-            if silence_start is None:
-                silence_start = time_in_buff
-            elif time_in_buff - silence_start > SILENCE_DURATION:
-                print("", flush=True) # 改行
-                # 無音区間で区切る処理
-                process_audio_chunk(audio_buffer)
-                audio_buffer = []
-                silence_start = None
-            #else:
-            #    print(time_in_buff - silence_start)
-                
+        if voice_on:
+            print(f"o {volume}\n", end='', flush=True)
         else:
-            #print(f"{volume}");
-            print("o", end='', flush=True)
-            silence_start = None
+            print(f". {volume}\r", end='', flush=True)
+
+        if ( status == 'A_waiting_start' ):
+            if ( voice_on ):
+                audio_buffer.append(data)
+                status = 'B_in_voice'
+            else:
+                pass # status = 'A_waiting_start'
+
+        elif ( status == 'B_in_voice' ):
+            if ( voice_on ):
+                audio_buffer.append(data)
+                voice_off_count = 0
+                # status = 'B_in_voice'
+            else:
+                audio_buffer.append(data)
+                voice_off_count = 1
+                status = 'C_counting'
+
+        elif ( status == 'C_counting' ):
+            if ( voice_on ):
+                audio_buffer.append(data)
+                voice_off_count = 0
+                status = 'B_in_voice'
+            else:
+                voice_off_count += 1
+                if ( voice_off_count >= Threshold ):
+                    # HACK 多分 actual_bufでよいはず。
+                    # actual_buf = audio_buffer[0:-voice_off_count]
+                    print(f"\nrecord: #{seqnum} {len(audio_buffer) * CHUNK_SIZE // SAMPLE_RATE} sec")
+                    #task_queue.put({'chunk':actual_buf, 'seq':seqnum})
+                    task_queue.put({'chunk':audio_buffer, 'seq':seqnum})
+                    seqnum += 1
+                    audio_buffer = []
+                    status = 'A_waiting_start'
+                else:
+                    pass # status = 'C_counting'
+        else:
+            raise 'unknown status' # HACK
 
     # end loop
+
+    task_queue.put(None)
+
     # オーディオストリームの終了処理
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
+    print("record() end")
 
-seqnum = 0
 
-def process_audio_chunk(chunk):
-    global seqnum
+# 文字起こし
+def transcript(task_queue, upload_queue):
 
-    # ここでオーディオチャンクを処理（例：文字起こし）
+    whisperso.initialize()
 
-    filename = f"./data/{seqnum}.wav"
-    seqnum += 1
+    while True:
+        task = task_queue.get()
+        if ( task == None ):
+            break
+        else:
+            chunk = task['chunk']
+            seqnum = task['seq']
+            #print(f"\ntranscript: #{task['seq']} start")
+            #print(f"({len(chunk)} chunks {len(chunk)*CHUNK_SIZE//SAMPLE_RATE} sec)")
+            data_int = np.frombuffer(b''.join(chunk), dtype=np.int16)
+            #data_float32 = data_int.astype(np.float32) / 32768.0
+            ret = whisperso.ondata(data_int, task['seq'])
+            if ( 0 < len(ret) ):
+                print(f"\ntranscript: #{task['seq']} {'~'.join(ret)}" )
+                upload_queue.put({'seq': seqnum, 'text': ret} )
+            else:
+                print(f"\ntranscript: #{task['seq']} was empty")
 
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(SAMPLE_WIDTH)
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(chunk))
-    wf.close()
 
-    print(f"{filename} ({len(chunk)} chunks {len(chunk)*CHUNK//RATE} sec)")
+    upload_queue.put(None)
+    whisperso.free()
+    print("transcript() end")
+
+# notionへのアップロード
+async def upload(upload_queue):
+   
+    print("upload() initilizing") 
+    notionif.init()
+
+    while True:
+        task = upload_queue.get()
+        if ( task == None ):
+            break
+        else:
+            seqnum = task['seq']
+            text = task['text'] # array
+            await notionif.upload(f"{'〜'.join(text)}（自動")
+            print(f"upload: #{seqnum} {text}")
+
+    print('upload() end')
+
+# uploadがasyncなのでthreadに渡すためラッパーをかける
+def upload_thread(upload_queue):
+    print("upload_thread initilizing") 
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(upload(upload_queue))
+    loop.close()
+    
 
 def main():
+    # keyboard
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
-    recorder()
-    
+
+    # queue
+    task_queue = queue.Queue()
+    upload_queue = queue.Queue()
+
+    # producer & consumer
+    recorder     = threading.Thread(target=record, args=(task_queue, ))
+    transcripter = threading.Thread(target=transcript, args=(task_queue, upload_queue))
+    uploader     = threading.Thread(target=upload_thread, args=(upload_queue, ))
+  
+    # 後段から開始
+    uploader.start() 
+    transcripter.start()
+    recorder.start()
+
+    # 前段から待ち
+    recorder.join()
+    transcripter.join()
+    uploader.join() 
+
 main()
 
 
---
+++ whisperso.py
@@ -0,0 +1,205 @@
+import ctypes
+#import pathlib
+
+# this is needed to read the WAV file properly from scipy.io import wavfile
+from scipy.io import wavfile
+
+import myenv
+# myenv.LIB_PATH
+# myenv.MODEL_PATH
+# myenv.TEST_TARGET
+
+# this needs to match the C struct in whisper.h
+class WhisperFullParams(ctypes.Structure):
+    _fields_ = [
+        ("strategy", ctypes.c_int),
+        #
+        ("n_threads", ctypes.c_int),
+        ("n_max_text_ctx", ctypes.c_int),
+        ("offset_ms", ctypes.c_int),
+        ("duration_ms", ctypes.c_int),
+        #
+        ("translate", ctypes.c_bool),
+        ("no_context", ctypes.c_bool),
+        ("no_timestamps", ctypes.c_bool),
+        ("single_segment", ctypes.c_bool),
+        ("print_special", ctypes.c_bool),
+        ("print_progress", ctypes.c_bool),
+        ("print_realtime", ctypes.c_bool),
+        ("print_timestamps", ctypes.c_bool),
+        #
+        ("token_timestamps", ctypes.c_bool),
+        ("thold_pt", ctypes.c_float),
+        ("thold_ptsum", ctypes.c_float),
+        ("max_len", ctypes.c_int),
+        ("split_on_word", ctypes.c_bool),
+        ("max_tokens", ctypes.c_int),
+        #
+        ("speed_up", ctypes.c_bool),
+        ("debug_mode", ctypes.c_bool),
+        ("audio_ctx", ctypes.c_int),
+        #
+        ("tbrz_enables", ctypes.c_bool),
+        #
+        ("initial_prompt", ctypes.c_char_p),
+        ("prompt_tokens", ctypes.c_void_p),
+        ("prompt_n_tokens", ctypes.c_int),
+        #
+        ("language", ctypes.c_char_p),
+        ("detect_language", ctypes.c_bool),
+        #
+        ("suppress_blank", ctypes.c_bool),
+        ("suppress_non_speech_tokens", ctypes.c_bool),
+        #
+        ("temperature", ctypes.c_float),
+        ("max_initial_ts", ctypes.c_float),
+        ("length_penalty", ctypes.c_float),
+        #
+        ("temperature_inc", ctypes.c_float),
+        ("entropy_thold", ctypes.c_float),
+        ("logprob_thold", ctypes.c_float),
+        ("no_speech_thold", ctypes.c_float),
+        #
+        # struct { int }
+        ("greedy", ctypes.c_int * 1),
+        #
+        # struct { int, float }
+        ("beam_search", ctypes.c_int * 3),
+        #
+        ("new_segment_callback", ctypes.c_void_p),
+        ("new_segment_callback_user_data", ctypes.c_void_p),
+        #
+        ("progress_callback", ctypes.c_void_p),
+        ("progress_callback_user_data", ctypes.c_void_p),
+        #
+        ("encoder_begin_callback", ctypes.c_void_p),
+        ("encoder_begin_callback_user_data", ctypes.c_void_p),
+        #
+        ("abort_callback", ctypes.c_void_p),
+        ("abort_callback_user_data", ctypes.c_void_p),
+        #
+        ("logits_filter_callback", ctypes.c_void_p),
+        ("logits_filter_callback_user_data", ctypes.c_void_p),
+        #
+        ("grammar_rules", ctypes.c_void_p),
+        ("n_grammer_rules", ctypes.c_size_t),
+        ("i_start_rule", ctypes.c_size_t),
+        ("grammar_penalty", ctypes.c_float),
+    ]
+
+
+whisper = None
+ctx = None
+params = None
+
+def initialize():
+    global whisper
+    global ctx
+    global params
+
+    libname = myenv.LIB_PATH 
+    whisper = ctypes.CDLL(libname)
+
+    # tell Python what are the return types of the functions
+    whisper.whisper_init_from_file.restype = ctypes.c_void_p
+    whisper.whisper_full_default_params.restype = WhisperFullParams
+    whisper.whisper_full_get_segment_text.restype = ctypes.c_char_p
+
+    # initialize whisper.cpp context
+    #ctx = whisper.whisper_init_from_file(fname_model.encode("utf-8"))
+    ctx = whisper.whisper_init_from_file(myenv.MODEL_PATH.encode("utf-8"))
+
+    # get default whisper parameters and adjust as needed
+    params = whisper.whisper_full_default_params()
+    params.print_special = False
+    params.print_progress = False
+    params.print_realtime = False
+    params.print_timestamps = False
+
+    #params.print_realtime = True
+    #params.print_progress = False
+    params.language = b'ja'
+
+    #params.no_speech_thold = 100 / 32768.0 # hack
+    params.temperature = 0.8
+
+def ondata(data, seqnum=None):
+    global whisper
+    global ctx
+    global params
+
+    # convert to 32-bit float
+    data = data.astype("float32") / 32768.0 #
+
+    # run the inference
+    result = whisper.whisper_full(
+        ctypes.c_void_p(ctx),
+        params,
+        data.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        len(data),
+    )
+
+    if result != 0:
+        print("Error: {}".format(result))
+        exit(1)
+
+    # print results from Python
+    n_segments = whisper.whisper_full_n_segments(ctypes.c_void_p(ctx))
+
+    ret = []
+
+    for i in range(n_segments):
+        #t0 = whisper.whisper_full_get_segment_t0(ctypes.c_void_p(ctx), i)
+        #t1 = whisper.whisper_full_get_segment_t1(ctypes.c_void_p(ctx), i)
+        txt = whisper.whisper_full_get_segment_text(ctypes.c_void_p(ctx), i)
+        decoded_text = txt.decode('utf-8')
+
+        ret.append(decoded_text)
+        #print(f"{seqnum}: {t0/1000.0:.3f} - {t1/1000.0:.3f} : {decoded_text}")
+
+    return ret
+
+def free():
+    global whisper
+    global ctx
+
+    # free the memory
+    whisper.whisper_free(ctypes.c_void_p(ctx))
+
+
+    
+
+if __name__ == "__main__":
+
+
+    # load WAV file
+    #samplerate, data = wavfile.read(fname_wav)
+    samplerate, data = wavfile.read(myenv.TEST_TARGET)
+
+    # convert to 32-bit float
+    data = data.astype("float32") / 32768.0
+
+    # run the inference
+    result = whisper.whisper_full(
+        ctypes.c_void_p(ctx),
+        params,
+        data.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        len(data),
+    )
+
+    if result != 0:
+        print("Error: {}".format(result))
+        exit(1)
+
+    # print results from Python
+    n_segments = whisper.whisper_full_n_segments(ctypes.c_void_p(ctx))
+    for i in range(n_segments):
+        t0 = whisper.whisper_full_get_segment_t0(ctypes.c_void_p(ctx), i)
+        t1 = whisper.whisper_full_get_segment_t1(ctypes.c_void_p(ctx), i)
+        txt = whisper.whisper_full_get_segment_text(ctypes.c_void_p(ctx), i)
+
+        print(f"{t0/1000.0:.3f} - {t1/1000.0:.3f} : {txt.decode('utf-8')}")
+
+
+
+
